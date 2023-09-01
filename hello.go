040f25643a6784a645404204432fb426194c32b8
ak2ie <mail@ak2ie.net>
2023-09-01 16:09:03 +0000

    add language

--- hello.go
+++ hello.go
@@ -2,10 +2,31 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, "+ name
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenshHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		return spanishHelloPrefix
+	case french:
+		return frenshHelloPrefix
+	default:
+		return englishHelloPrefix
+	}
 }
 
 func main()  {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
